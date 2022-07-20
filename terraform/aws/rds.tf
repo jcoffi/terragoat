a2da0bb77d746fbfc66330e81b52518f4dd8a401
Justin Coffi <jcoffi+github@gmail.com>
2022-07-20 01:01:28 -0700

    Update rds.tf

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -2,6 +2,8 @@ resource "aws_rds_cluster" "app1-rds-cluster" {
   cluster_identifier      = "app1-rds-cluster"
   #allocated_storage       = 10
   backup_retention_period = 0
+  master_username         = "admin"
+  master_password         = var.password
   db_subnet_group_name    = "${aws_db_subnet_group.default.name}"
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
