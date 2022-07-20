bbc3d507fe36a8e3cdd5f097ba0f75a9e7f5fe32
Justin Coffi <jcoffi+github@gmail.com>
2022-07-19 22:13:16 -0700

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -91,7 +91,7 @@ resource "aws_s3_bucket" "operations" {
 resource "aws_s3_bucket" "data_science" {
   # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
-  aws_s3_bucket_acl    = "private"
+  acl    = "private"
   versioning {
     enabled = true
   }
