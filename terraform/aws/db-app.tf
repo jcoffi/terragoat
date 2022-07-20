28a4b286046eac30ffd86cf8c439cc4fd01a035e
Justin Coffi <jcoffi+github@gmail.com>
2022-07-20 00:57:59 -0700

    Update db-app.tf

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -10,8 +10,8 @@ resource "aws_db_instance" "default" {
   engine_version          = "8.0" # Latest major version 
   instance_class          = "db.t3.micro"
   allocated_storage       = "20"
-  username                = "admin"
-  password                = var.password
+  master_username         = "admin"
+  master_password         = var.password
   apply_immediately       = true
   multi_az                = false
   backup_retention_period = 0
