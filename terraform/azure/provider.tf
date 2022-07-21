ddd9ef963a8be7954968c4cdf5d15c3bf4a8f1b3
Justin Coffi <jcoffi+github@gmail.com>
2022-07-20 21:56:38 -0700

    Update provider.tf

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
      azurerm = {
      source  = "hashicorp/azurerm"
-     version = "=2.97.0"
+     version = "=2.27.0"
     }
   }
   required_version = ">=0.12.0"
