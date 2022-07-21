5ee31cc1b8254fedc901014b1b49273a907078e0
Justin Coffi <jcoffi+github@gmail.com>
2022-07-20 22:19:06 -0700

    Update variables.tf

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -13,3 +13,25 @@ variable "environment" {
   default     = "dev"
   description = "Must be all lowercase letters or numbers"
 }
+
+variable "company_name" {
+  default = "acme"
+}
+
+locals {
+  resource_prefix = {
+    value = "${var.company_name}-${var.environment}"
+  }
+}
+
+variable "dbname" {
+  type        = string
+  description = "Name of the Database"
+  default     = "db1"
+}
+
+variable "password" {
+  type        = string
+  description = "Database password"
+  default     = "Aa1234321Bb"
+}
